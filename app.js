b51886f8979f6d68809d7312e94ea7485fe7294b
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-10-22 13:26:17 -0700

    dinf

---
+++ app.js
@@ -0,0 +1,8 @@
+const pixels = document.getElementById('pixels-text');
+
+const getWidth = ()=>{
+    let width = document.documentElement.clientWidth;
+    pixels.innerText = `${width}px`;
+}
+getWidth();
+window.addEventListener('resize', getWidth);
